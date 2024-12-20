cceee335219e5b0887122181fe81cc7839e54a74
KenzaAsmani-dev <kenzaasmani8@gmail.com>
2024-12-20 19:49:22 +0100

    Feat (Add form create) : Create

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Create() {
+
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+
+const navigate= useNavigate();
+
+const handleSubmit = () => {
+
+  navigate('/');
+
+}
+
   return (
-    <div>
-      Create
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+      <div className='w-50 border bg-secondary text-white p-5'>
+        <h3>Ajouter un nouveau utilisateur</h3>
+        <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name : </label>
+          {/* e.target.value récupère les nouvelles valeurs saisies daans le champ / setName pour mettre à jour la valeur initiale avec la nouvelle valeur*/}
+          <input type="text" name='name' className='form-control' value={name} onChange={(e)=> setName(e.target.value)} placeholder='Entrez votre nom complet'/>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email : </label>
+          <input type="email" name='email' className='form-control' value={email} onChange={(e)=> setEmail(e.target.value)}  placeholder='Entrez votre email'/>
+        </div> <br />
+
+        <button className='btn btn-info' >Envoyer</button>
+
+        </form>
+      </div>
+      
     </div>
   )
 }
--- src/Update.jsx
+++ src/Update.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 function Update() {
   return (
-    <div>
-      Update
+    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+    <div className='w-50 border bg-secondary text-white p-5'>
+      <h3>Modifier un utilisateur</h3>
+      <form>
+      <div>
+        <label htmlFor="name">Name : </label>
+        <input type="text" name='name' className='form-control' placeholder='Entrez votre nom complet'/>
+      </div>
+
+      <div>
+        <label htmlFor="email">Email : </label>
+        <input type="email" name='email' className='form-control' placeholder='Entrez votre email'/>
+      </div> <br />
+
+      <button className='btn btn-info'>Modifier</button>
+
+      </form>
     </div>
+  </div>
   )
 }
 
